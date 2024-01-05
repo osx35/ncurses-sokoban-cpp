91a51287419b23d32228783e067c6f0f68a8e7c0
oskarPC <oskarek.2005.ok@gmail.com>
2024-01-05 12:58:21 +0100

    Small changes to mapa.h

--- mapa.h
+++ mapa.h
@@ -4,57 +4,59 @@
 #ifndef MAPA_H
 #define MAPA_H
 
-struct MapSize {
+class Map {
+private:
     int rows;
     int cols;
+
+public:
+    Map() : rows(0), cols(0) {}
+
+    void sprawdzRozmiar() {
+        std::fstream mapa;
+        mapa.open("mapa.txt", std::ios::in);
+
+        if (!mapa.good()) {
+            std::cout << "Nie ma takiego pliku";
+            return;
+        }
+
+        rows = 0;
+        cols = 0;
+
+        std::string linia;
+        while (getline(mapa, linia)) {
+            ++rows;
+            if (rows == 1) {
+                cols = linia.size();
+            }
+        }
+
+        mapa.close();
+    }
+
+    void printMap() {
+        std::fstream mapa;
+        mapa.open("mapa.txt", std::ios::in);
+        std::string linia;
+        char mapaArray[rows][cols];
+
+        for (int i = 0; i < rows; ++i) {
+            getline(mapa, linia);
+
+            for (int j = 0; j < cols; ++j) {
+                mapaArray[i][j] = linia[j];
+            }
+        }
+
+        mapa.close();
+
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                std::cout << mapaArray[i][j];
+            }
+            std::cout << '\n';
+        }
+    }
 };
-
-MapSize sprawdzRozmiar() {
-    std::fstream mapa;
-    mapa.open("mapa.txt", std::ios::in);
-
-    if (!mapa.good()) {
-        std::cout << "Nie ma takiego pliku";
-        return {0, 0};  
-    }
-
-    MapSize size;
-    size.rows = 0;
-    size.cols = 0;
-
-    std::string linia;
-    while (getline(mapa, linia)) {
-        ++size.rows;
-        if (size.rows == 1) {
-            size.cols = linia.size();
-        }
-    }
-
-    mapa.close();
-    return size;
-}
-
-void printMap(int rows, int cols) {
-    std::fstream mapa;
-    mapa.open("mapa.txt", std::ios::in);
-    std::string linia;
-    char mapaArray[rows][cols];
-
-    for (int i = 0; i < rows; ++i) {
-        getline(mapa, linia);
-
-        for (int j = 0; j < cols; ++j) {
-            mapaArray[i][j] = linia[j];
-        }
-    }
-
-    mapa.close();
-
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            std::cout << mapaArray[i][j];
-        }
-        std::cout << '\n';
-    }
-}
-#endif 
+#endif
